6cdca26ce7fe153b8808268753ece944d6e833da
Anton Kislyakov <antonkis2012@gmail.com>
2023-02-07 15:08:30 +0300

    кепреуркп

--- GitApplication/Program.cs
+++ GitApplication/Program.cs
@@ -3,15 +3,7 @@
 namespace GitApplication
 {
     internal class Program
-    {
-        static void Main(string[] args)
-        {
-            
-        }
-
-        public static void MatrixB(int w, int h) 
-        {
-            Console.WriteLine("hello");
+    {nrjnjrngnrgjlwrnjlgwrngonsole.WriteLine("April, 20");
         }
     }
 }
